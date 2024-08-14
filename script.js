377c66d3df52b0f9f97a1efaf7d96ade133b54d3
Luis Irizarry <lirizarry6178@gmail.com>
2024-08-13 22:57:37 -0400

    Added more fruits

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const input = document.querySelector("#fruit");
 const suggestions = document.querySelector(".suggestions ul");
 
-const fruit = ["Apple", "Apricot", "Avocado 🥑", "Banana", "Bilberry", "Blackberry", "Blackcurrant", "Blueberry", "Boysenberry", "Currant", "Cherry", "Coconut", "Cranberry", "Cucumber", "Custard apple", 
-	"Damson", "Date", "Dragonfruit", "Durian", "Elderberry", "Feijoa", "Fig", "Gooseberry", "Grape", "Raisin", "Grapefruit", "Guava", "Honeyberry", "Huckleberry", "Jabuticaba", "Jackfruit", "Jambul", 
-	"Juniper berry", "Kiwifruit", "Kumquat", "Lemon", "Lime", "Loquat", "Longan", "Lychee", "Mango", "Mangosteen", "Marionberry", "Melon", "Cantaloupe", "Honeydew", "Watermelon", "Miracle fruit", "Mulberry", 
-	"Nectarine", "Nance", "Olive", "Orange", "Clementine", "Mandarine", "Tangerine", "Papaya", "Passionfruit", "Peach", "Pear", "Persimmon", "Plantain", "Plum", "Pineapple", "Pomegranate", "Pomelo", "Quince", 
-	"Raspberry", "Salmonberry", "Rambutan", "Redcurrant", "Salak", "Satsuma", "Soursop", "Star fruit", "Strawberry", "Tamarillo", "Tamarind", "Yuzu"];
+const fruit = [
+    "Acai Berry", "Apple", "Apricot", "Avocado 🥑", "Baobab", "Banana", "Bilberry", "Blackberry", "Blackcurrant", "Blueberry", "Boysenberry", "Cactus Fruit", 
+    "Camu Camu", "Cantaloupe", "Cherimoya", "Cherry", "Coconut", "Cranberry", "Cucumber", "Custard apple", "Date", "Dragonfruit", "Durian", "Elderberry", 
+	"Feijoa", "Fig", "Gooseberry", "Grape", "Guava", "Honeyberry", "Honeydew", "Huckleberry", "Jabuticaba", "Jackfruit", "Jambul", "Jujube", "Kiwifruit", 
+    "Kumquat", "Lemon", "Lime", "Loquat", "Longan", "Lychee", "Mango", "Mangosteen", "Marionberry", "Melon", "Miracle fruit", "Mulberry", "Nance", 
+    "Noni", "Olive", "Orange", "Papaya", "Passionfruit", "Peach", "Pear", "Persimmon", "Pitanga", "Plantain", "Plum", "Pineapple", "Pomegranate", 
+    "Pomelo", "Quince", "Raspberry", "Rambutan", "Redcurrant", "Salak", "Salmonberry", "Satsuma", "Sapodilla", "Soursop", "Starfruit", "Strawberry", "Tamarillo", "Tamarind", "Yuzu", "Ziziphus"
+];
 
 function search(str) {
 	let results = [];
